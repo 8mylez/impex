77c62dec5df806190fe59e1226d6fd6a9b72596d
Dustin Simon <dustin.simon99@gmail.com>
2023-08-01 16:21:17 +0200

    Load sequences from file

--- src/Sequence/Registry/Loader/FileSequenceLoader.php
+++ src/Sequence/Registry/Loader/FileSequenceLoader.php
@@ -4,7 +4,9 @@ namespace Dustin\ImpEx\Sequence\Registry\Loader;
 
 use Dustin\Exception\ConstraintViolationException;
 use Dustin\ImpEx\Sequence\Registry\Config\SectionDefinition;
+use Dustin\ImpEx\Sequence\Registry\Config\SequenceDefinition;
 use Dustin\ImpEx\Sequence\Registry\Config\SequenceDefinitionContainer;
+use Dustin\ImpEx\Sequence\Registry\Validation\SectionValidator;
 use Dustin\ImpEx\Sequence\Registry\Validation\SequenceClass;
 use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\Serializer\Encoder\EncoderInterface;
@@ -13,10 +15,10 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\YamlEncoder;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Validator\Constraints\All;
-use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -50,8 +52,9 @@ class FileSequenceLoader implements SequenceLoaderInterface
 
         $decoded = $this->serializer->decode(file_get_contents($this->file), $this->format);
         $sequences = new SequenceDefinitionContainer();
-        $violationList = new ConstraintViolationList();
+        $violations = new ConstraintViolationList();
         $constraints = $this->getConstraints();
+        $sequenceIds = [];
 
         foreach ($decoded as $id => $data) {
             $data = array_merge(
@@ -59,25 +62,35 @@ class FileSequenceLoader implements SequenceLoaderInterface
                 ['id' => $id]
             );
 
+            $sequenceViolations = $this->validator->startContext()
+                ->atPath($id)
+                ->validate($data, $constraints)
+                ->getViolations();
+
+            if (count($sequenceViolations) > 0) {
+                foreach ($sequenceViolations as $violation) {
+                    $violations->add($violation);
+                }
+
+                continue;
+            }
+
             try {
-                foreach ($this->createSequences($data, $constraints) as $sequence) {
+                foreach ($this->createSequences($data, $sequenceIds, $id) as $sequence) {
                     $sequences->add($sequence);
                 }
-            } catch (ConstraintViolationException $exception) {
+            } catch (ConstraintViolationException $e) {
+                foreach ($e->getViolations() as $violation) {
+                    $violations->add($violation);
+                }
             }
         }
-    }
-
-    protected function createSequences(array $data, array|Collection $constraints): \Generator
-    {
-        $violations = $this->validator->validate($data, $constraints);
 
         if (count($violations) > 0) {
-            throw new ConstraintViolationException($violations, $data);
+            throw new ConstraintViolationException($violations, $decoded);
         }
 
-        foreach ($data as $sequence) {
-        }
+        return $sequences;
     }
 
     protected function getConstraints(): array|Collection
@@ -86,13 +99,59 @@ class FileSequenceLoader implements SequenceLoaderInterface
             'allowExtraFields' => false,
             'fields' => [
                 'id' => new NotBlank(),
-                'class' => new NotBlank(), new SequenceClass(),
+                'class' => [new NotBlank(), new SequenceClass()],
                 'sections' => new All([
-                    'type' => [new NotBlank(), new Choice([], [SectionDefinition::TYPE_SECTION, SectionDefinition::TYPE_SEQUENCE])],
-                    'id' => new NotBlank(),
-                    'priority' => new Type('integer'),
+                    new Callback([SectionValidator::class, 'validate']),
                 ]),
             ],
         ]);
+    }
+
+    private function createSequences(array $data, array &$usedIds, string $path): \Generator
+    {
+        $violations = new ConstraintViolationList();
+        $id = $data['id'];
+
+        if (isset($usedIds[$id])) {
+            $violations->add(new ConstraintViolation(
+                'Duplicate sequence ids.',
+                '',
+                ['id' => $id],
+                $data,
+                $path.'[id]',
+                $id
+            ));
+        }
+
+        $usedIds[$id] = $id;
+
+        $definition = new SequenceDefinition([
+            'id' => $data['id'],
+            'class' => $data['class'],
+        ]);
+
+        foreach ($data['sections'] as $index => $sectionData) {
+            $definition->getSections()->add(new SectionDefinition([
+                'id' => $sectionData['id'],
+                'type' => $sectionData['type'],
+                'priority' => (int) $sectionData['priority'],
+            ]));
+
+            if ($sectionData['type'] === SectionDefinition::TYPE_SEQUENCE && SequenceDefinitionDetector::isSequenceDefinition($sectionData)) {
+                try {
+                    yield from $this->createSequences($sectionData, $usedIds, $path.'[sections]['.$index.']');
+                } catch (ConstraintViolationException $e) {
+                    foreach ($e->getViolations() as $violation) {
+                        $violations->add($violation);
+                    }
+                }
+            }
+        }
+
+        if (count($violations) > 0) {
+            throw new ConstraintViolationException($violations, $data);
+        }
+
+        yield $definition;
     }
 }
