0a8bd8a3798052b92ce1810200e212d0293e6ca9
Dustin Simon <dustin.simon99@gmail.com>
2023-10-09 11:46:36 +0200

    Add PreloadSequenceRepository

--- src/Sequence/Registry/SequenceRepository.php
+++ src/Sequence/Registry/SequenceRepository.php
@@ -4,74 +4,31 @@ namespace Dustin\ImpEx\Sequence\Registry;
 
 use Dustin\Encapsulation\ObjectMapping;
 use Dustin\ImpEx\Sequence\Registry\Config\SequenceDefinition;
-use Dustin\ImpEx\Sequence\Registry\Loader\SequenceLoaderInterface;
 
 class SequenceRepository implements SequenceRepositoryInterface
 {
-    /**
-     * @var SequenceLoaderInterface[]
-     */
-    private $loaders = [];
-
     /**
      * @var ObjectMapping
      */
     private $sequences;
 
-    private bool $loaded = false;
-
-    private bool $isLoading = false;
-
-    public function __construct(iterable $loaders)
+    public function __construct()
     {
-        foreach ($loaders as $loader) {
-            $this->addLoader($loader);
-        }
-
         $this->sequences = ObjectMapping::create(SequenceDefinition::class);
-    }
-
-    public function addLoader(SequenceLoaderInterface $loader): void
-    {
-        $this->loaders[] = $loader;
     }
 
     public function addSequence(SequenceDefinition $definition): void
     {
-        $this->loadSequences();
-
         $this->sequences->set($definition->getId(), $definition);
     }
 
     public function getSequence(string $id): ?SequenceDefinition
     {
-        $this->loadSequences();
-
         return $this->sequences->get($id);
     }
 
     public function hasSequence(string $id): bool
     {
-        $this->loadSequences();
-
         return $this->sequences->has($id);
-    }
-
-    private function loadSequences(): void
-    {
-        if ($this->loaded === true || $this->isLoading === true) {
-            return;
-        }
-
-        $this->isLoading = true;
-
-        foreach ($this->loaders as $loader) {
-            foreach ($loader->load() as $sequence) {
-                $this->addSequence($sequence);
-            }
-        }
-
-        $this->loaded = true;
-        $this->isLoading = false;
     }
 }
