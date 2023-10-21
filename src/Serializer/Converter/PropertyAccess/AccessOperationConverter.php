283803193e5af612ae43c3284186cfcc44cf6116
Dustin Simon <dustin.simon@8mylez.com>
2023-10-21 18:10:21 +0200

    Add AccessOperationConverter

---
+++ src/Serializer/Converter/PropertyAccess/AccessOperationConverter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dustin\ImpEx\Serializer\Converter\PropertyAccess;
+
+use Dustin\ImpEx\PropertyAccess\Exception\InvalidDataException;
+use Dustin\ImpEx\PropertyAccess\Exception\NotAccessableException;
+use Dustin\ImpEx\PropertyAccess\Exception\OperationNotSupportedException;
+use Dustin\ImpEx\PropertyAccess\Exception\PropertyNotFoundException;
+use Dustin\ImpEx\PropertyAccess\Operation\AccessOperation;
+use Dustin\ImpEx\Serializer\Converter\ConversionContext;
+use Dustin\ImpEx\Serializer\Converter\UnidirectionalConverter;
+use Dustin\ImpEx\Serializer\Exception\AttributeConversionException;
+
+class AccessOperationConverter extends UnidirectionalConverter
+{
+    public function __construct(private AccessOperation $operation, private mixed $writeValue = null)
+    {
+    }
+
+    public function convert(mixed $value, ConversionContext $context): mixed
+    {
+        $writeValue = $this->writeValue;
+
+        if (is_callable($writeValue)) {
+            $writeValue = $writeValue($value, $context);
+        }
+
+        try {
+            $this->operation->execute($value, $writeValue);
+        } catch (InvalidDataException|NotAccessableException|OperationNotSupportedException|PropertyNotFoundException $exception) {
+            throw new AttributeConversionException($context->getPath(), $context->getRootData(), $exception->getErrorCode(), $exception->getMessage(), $exception->getParameters());
+        }
+    }
+}
