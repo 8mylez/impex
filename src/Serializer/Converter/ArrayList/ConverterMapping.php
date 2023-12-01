c2aafbc2099f6a91b1467990d177b1da207899c3
Dustin Simon <dustin.simon99@gmail.com>
2023-12-01 21:16:35 +0100

    ConverterMapping can now iterate over converters

--- src/Serializer/Converter/ArrayList/ConverterMapping.php
+++ src/Serializer/Converter/ArrayList/ConverterMapping.php
@@ -12,6 +12,8 @@ use Dustin\ImpEx\Util\Type;
 
 class ConverterMapping extends BidirectionalConverter
 {
+    public const ITERATE_CONVERTERS = 'iterate_converters';
+
     private $converters = [];
 
     public function __construct(array $converters, string ...$flags)
@@ -39,8 +41,50 @@ class ConverterMapping extends BidirectionalConverter
             return null;
         }
 
+        if ($this->hasFlags(self::ITERATE_CONVERTERS)) {
+            return $this->normalizeWithConverters($data, $context);
+        }
+
         $this->ensureType($data, Type::ARRAY, $context);
 
+        return $this->normalizeWithValue($data, $context);
+    }
+
+    public function denormalize(mixed $data, ConversionContext $context): array|null
+    {
+        if ($this->hasFlags(self::SKIP_NULL) && $data === null) {
+            return null;
+        }
+
+        if ($this->hasFlags(self::ITERATE_CONVERTERS)) {
+            return $this->denormalizeWithConverters($data, $context);
+        }
+
+        $this->ensureType($data, Type::ARRAY, $context);
+
+        return $this->denormalizeWithValue($data, $context);
+    }
+
+    protected function normalizeWithConverters(mixed $value, ConversionContext $context): array
+    {
+        $converted = [];
+        $exceptions = new AttributeConversionExceptionStack($context->getPath(), $context->getRootData());
+
+        foreach ($this->converters as $key => $converter) {
+            try {
+                $converted[$key] = $converter->normalize($value, $context->subContext(new Path([$key])));
+            } catch (AttributeConversionException $e) {
+                $exceptions->add($e);
+            }
+        }
+
+        $exceptions->throw();
+
+        return $converted;
+    }
+
+    protected function normalizeWithValue(array $data, ConversionContext $context): array
+    {
         $converted = [];
         $exceptions = new AttributeConversionExceptionStack($context->getPath(), $context->getRootData());
 
@@ -59,14 +103,26 @@ class ConverterMapping extends BidirectionalConverter
         return $converted;
     }
 
-    public function denormalize(mixed $data, ConversionContext $context): array|null
+    protected function denormalizeWithConverters(mixed $value, ConversionContext $context): array
     {
-        if ($this->hasFlags(self::SKIP_NULL) && $data === null) {
-            return null;
+        $converted = [];
+        $exceptions = new AttributeConversionExceptionStack($context->getPath(), $context->getRootData());
+
+        foreach ($this->converters as $key => $converter) {
+            try {
+                $converted[$key] = $converter->denormalize($value, $context->subContext(new Path([$key])));
+            } catch (AttributeConversionException $e) {
+                $exceptions->add($e);
+            }
         }
 
-        $this->ensureType($data, Type::ARRAY, $context);
+        $exceptions->throw();
 
+        return $converted;
+    }
+
+    protected function denormalizeWithValue(array $data, ConversionContext $context): array
+    {
         $converted = [];
         $exceptions = new AttributeConversionExceptionStack($context->getPath(), $context->getRootData());
 
