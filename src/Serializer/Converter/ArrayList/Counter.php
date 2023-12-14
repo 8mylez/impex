6148466fb2a201bb90e795da1fa8ba83868144e2
Dustin Simon <dustin.simon99@gmail.com>
2023-12-14 15:19:13 +0100

    Remove CountStrategy

--- src/Serializer/Converter/ArrayList/Counter.php
+++ src/Serializer/Converter/ArrayList/Counter.php
@@ -4,23 +4,17 @@ namespace Dustin\ImpEx\Serializer\Converter\ArrayList;
 
 use Dustin\ImpEx\Serializer\Converter\ConversionContext;
 use Dustin\ImpEx\Serializer\Converter\UnidirectionalConverter;
-use Dustin\ImpEx\Util\Type;
 
 class Counter extends UnidirectionalConverter
 {
-    public function __construct(private CountStrategy $strategy, string ...$flags)
-    {
-        parent::__construct(...$flags);
-    }
-
     public function convert(mixed $value, ConversionContext $context): mixed
     {
         if ($this->hasFlags(self::SKIP_NULL) && $value === null) {
             return null;
         }
 
-        $this->ensureType($value, Type::ARRAY, $context);
+        $this->ensureType($value, \Countable::class, $context);
 
-        return $this->strategy->count($value, $context);
+        return count($value);
     }
 }
--- src/Serializer/Converter/ArrayList/DistinctCounter.php
+++ src/Serializer/Converter/ArrayList/DistinctCounter.php
@@ -4,14 +4,21 @@ namespace Dustin\ImpEx\Serializer\Converter\ArrayList;
 
 use Dustin\ImpEx\PropertyAccess\Path;
 use Dustin\ImpEx\Serializer\Converter\ConversionContext;
+use Dustin\ImpEx\Serializer\Converter\UnidirectionalConverter;
 use Dustin\ImpEx\Serializer\Exception\AttributeConversionExceptionStack;
 use Dustin\ImpEx\Serializer\Exception\TypeConversionException;
 use Dustin\ImpEx\Util\Type;
 
-class DistinctCountStrategy extends CountStrategy
+class DistinctCounter extends UnidirectionalConverter
 {
-    public function count(array $data, ConversionContext $context): array
+    public function convert(mixed $data, ConversionContext $context): mixed
     {
+        if ($this->hasFlags(self::SKIP_NULL) && $data === null) {
+            return null;
+        }
+
+        $this->ensureType($data, Type::ARRAY, $context);
+
         $exceptions = new AttributeConversionExceptionStack($context->getPath(), $context->getRootData());
 
         foreach ($data as $key => $value) {
